d28299a5288b6c1cf3744b362c00eab39f1533d7
Михаил <misha13022008@gmail.com>
2023-05-18 18:55:35 +0300

    insert, removeAt, contains, getFirst, clear, length & getLast

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ class LinkedList {
 
     addToEnd(value) {
         if (this.length == 0) {
-            this.first = this.last = new RNode(value, null)
+            this.first = this.last = new RNode(value)
             this.length++
             return
         }
 
-        this.last.next = new RNode(value, null)
+        this.last.next = new RNode(value)
         this.last = this.last.next
         this.length++
     }
@@ -23,16 +23,61 @@ class LinkedList {
         return current
     }
 
-    getFirst = () => this.first
+    insert(index, value) {
+        value = new RNode(value)
 
-    getLast = () => this.first
+        if (index == 0)
+        {
+            value.next = this.first
+            this.first = value
+            this.length++
+            return
+        }
+        
+        value.next = this.getByIndex(index)
+        this.getByIndex(index - 1).next = value
+        this.length++
+    }
+
+    removeAt(index) {
+        if (index == 0)
+        {
+            this.first = this.first.next
+            this.length--
+            return
+        }
+        
+        this.getByIndex(index - 1).next = this.getByIndex(index).next
+        this.length--
+    }
+
+    containsValue(value) {
+        let current = this.first
+
+        for (let i = 0; i < this.length; i++) {
+            if (current.value == value)
+                return true
+            current = current.next
+        }
+
+        return false
+    }
+
+    clearList()
+    {
+        this.first = null
+        this.length = 0
+    }
+
+    getFirst = () => this.first
+    getLast = () => this.last
 }
 
 class RNode {
     value
     next
 
-    constructor(value, next) {
+    constructor(value, next = null) {
         this.value = value
         this.next = next
     }
@@ -42,6 +87,42 @@ const list = new LinkedList()
 
 const addToEnd = (value) => {
     list.addToEnd(value)
+    showList(list)
+}
+
+const addFirst = (value) => {
+    list.insert(0, value)
+    showList(list)
+}
+
+const clearList = () => {
+    list.clearList()
+    showList(list)
+}
+
+const insert = (ind, value) => {
+    list.insert(ind, value)
+    showList(list)
+}
+
+const length = () => {
+    alert(list.length)
+}
+
+const getFirst = () => {
+    alert(list.getFirst().value)
+}
+
+const getLast = () => {
+    alert(list.getLast().value)
+}
+
+const containsValue = (value) => {
+    alert(list.containsValue(value))
+}
+
+const removeAt = (ind) => {
+    list.removeAt(ind)
     showList(list)
 }
 
